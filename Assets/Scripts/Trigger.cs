7613b2ccc836b6341c390f6bf745fa240bb8cb67
sam <skb502@nyu.edu>
2020-09-22 14:25:30 -0400

    stage 2 complete

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -12,6 +12,7 @@ public class Trigger : MonoBehaviour
     //runs when something with a Rigidbody2D enters the trigger
     void OnTriggerEnter2D(Collider2D activator) {
         Debug.Log ("entered");
+        bool playerMove = false;
         if (activator.gameObject.tag == "Player") {
             Debug.Log("player entered");
             cameraFollow = false;
@@ -25,11 +26,19 @@ public class Trigger : MonoBehaviour
                     camera.transform.position = new Vector3(0f, -19f, -10f);
                     break;
 
+                case 2:
+                    camera.GetComponent<Camera>().orthographicSize = 38;
+                    camera.transform.position = new Vector3(45f, -112f, -10f);
+                    break;
+
                 default:
                     Debug.Log("machineStage out of range");
                     break;
 
             }
+        } else if (activator.gameObject.tag == "ColoredCircle" && !playerMove) {
+            circle.GetComponent<ConstantForce2D>().torque = -500f;
+            playerMove = true;
         }
     }
 
@@ -38,7 +47,7 @@ public class Trigger : MonoBehaviour
         //make sure the camera has something to follow
         if (cameraFollow) {
             camera.transform.position = new Vector3 
-                (circle.transform.position.x, circle.transform.position.y, -10);
+                (circle.transform.position.x, circle.transform.position.y, -10f);
         }
     }
 }
