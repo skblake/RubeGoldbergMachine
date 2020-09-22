a339e6531bacefc73e35e0b36b76924247cdc773
sam <skb502@nyu.edu>
2020-09-22 12:30:40 -0400

    circle prefab

---
+++ Assets/Scripts/CircleColor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleColor : MonoBehaviour
+{
+    //Assign the object a random color from the color palette 
+    void Start()
+    {
+        
+    }
+
+}
